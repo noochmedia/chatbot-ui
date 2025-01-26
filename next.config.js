e196aa786dba8b6a628d54a77ed1e2dce6058703
noochmedia <inspirefilms@me.com>
2025-01-25 20:55:59 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
+  enabled: process.env.ANALYZE === "true",
+});
 
 const withPWA = require("next-pwa")({
-  dest: "public"
-})
+  dest: "public",
+});
 
 module.exports = withBundleAnalyzer(
   withPWA({
@@ -13,20 +13,51 @@ module.exports = withBundleAnalyzer(
       remotePatterns: [
         {
           protocol: "http",
-          hostname: "localhost"
+          hostname: "localhost",
         },
         {
           protocol: "http",
-          hostname: "127.0.0.1"
+          hostname: "127.0.0.1",
         },
         {
           protocol: "https",
-          hostname: "**"
-        }
-      ]
+          hostname: "**",
+        },
+      ],
+    },
+    headers: async () => {
+      return [
+        {
+          source: "/(.*)",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: `
+                default-src 'self'; 
+                script-src 'self' 'unsafe-inline' 'unsafe-eval'; 
+                connect-src 'self' https://gygbxgpvfsvihxbmahcy.supabase.co; 
+                img-src 'self' https://gygbxgpvfsvihxbmahcy.supabase.co data:; 
+                frame-src 'self';
+              `,
+            },
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "*",
+            },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET, POST, PUT, DELETE, OPTIONS",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "X-Requested-With, Content-Type, Authorization",
+            },
+          ],
+        },
+      ];
     },
     experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
+      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+    },
   })
-)
+);
